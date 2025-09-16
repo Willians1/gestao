fb25994d802cfb78e97864ba55bb7de74bf22125
Willians <willians@example.com>
2025-09-16 10:52:04 -0300

    revert(frontend): voltar a usar AppClean no index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './AppClean';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
